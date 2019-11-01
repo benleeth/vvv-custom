252a3908fef2e0ede35f56b30cffa7c22a8df3e9
Ben Leeth <ben@designontap.com>
2019-10-31 22:20:31 -0400

    Update 10/31/19
    Signed-off-by: Ben Leeth <ben@designontap.com>

--- provision/vvv-init.sh
+++ provision/vvv-init.sh
@@ -1,36 +1,66 @@
 #!/usr/bin/env bash
 # Provision WordPress Stable
 
+set -eo pipefail
+
+echo " * Custom site template provisioner - downloads and installs a copy of WP stable for testing, building client sites, etc"
+
 # fetch the first host as the primary domain. If none is available, generate a default using the site name
+DOMAIN=`get_primary_host "${VVV_SITE_NAME}".test`
+SITE_TITLE=`get_config_value 'site_title' "${DOMAIN}"`
+WP_VERSION=`get_config_value 'wp_version' 'latest'`
+WP_LOCALE=`get_config_value 'locale' 'en_US'`
+WP_TYPE=`get_config_value 'wp_type' "single"`
 DB_NAME=`get_config_value 'db_name' "${VVV_SITE_NAME}"`
-DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*-]/}
+DB_NAME=${DB_NAME//[\\\/\.\<\>\:\"\'\|\?\!\*]/}
 
 # Make a database, if we don't already have one
 echo -e "\nCreating database '${DB_NAME}' (if it's not already there)"
-mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME}"
-mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON ${DB_NAME}.* TO wp@localhost IDENTIFIED BY 'wp';"
+mysql -u root --password=root -e "CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`"
+echo -e "\nGranting the wp user priviledges to the '${DB_NAME}' database"
+mysql -u root --password=root -e "GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO wp@localhost IDENTIFIED BY 'wp';"
 echo -e "\n DB operations done.\n\n"
 
-# Nginx Logs
-mkdir -p ${VVV_PATH_TO_SITE}/log
-touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
-touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
 
-# Create public_html directory
-mkdir -p ${VVV_PATH_TO_SITE}/public_html
+echo "Setting up the log subfolder for Nginx logs"
+noroot mkdir -p ${VVV_PATH_TO_SITE}/log
+noroot touch ${VVV_PATH_TO_SITE}/log/nginx-error.log
+noroot touch ${VVV_PATH_TO_SITE}/log/nginx-access.log
 
-cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf.tmpl" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+echo "Creating public_html folder if it doesn't exist already"
+noroot mkdir -p ${VVV_PATH_TO_SITE}/public_html
 
-if [ -n "$(type -t is_utility_installed)" ] && [ "$(type -t is_utility_installed)" = function ] && `is_utility_installed core tls-ca`; then
-  VVV_CERT_DIR="/srv/certificates"
-  # On VVV 2.x we don't have a /srv/certificates mount, so switch to /vagrant/certificates
-  codename=$(lsb_release --codename | cut -f2)
-  if [[ $codename == "trusty" ]]; then # VVV 2 uses Ubuntu 14 LTS trusty
-    VVV_CERT_DIR="/vagrant/certificates"
-  fi
-  sed -i "s#{{TLS_CERT}}#ssl_certificate ${VVV_CERT_DIR}/${VVV_SITE_NAME}/dev.crt;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-  sed -i "s#{{TLS_KEY}}#ssl_certificate_key ${VVV_CERT_DIR}/${VVV_SITE_NAME}/dev.key;#" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+echo "Copying the sites Nginx config template"
+if [ -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" ]; then
+  echo "A vvv-nginx-custom.conf file was found"
+  cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-custom.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 else
-    sed -i "s#{{TLS_CERT}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
-    sed -i "s#{{TLS_KEY}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+  echo "Using the default vvv-nginx-default.conf, to customize, create a vvv-nginx-custom.conf"
+  cp -f "${VVV_PATH_TO_SITE}/provision/vvv-nginx-default.conf" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
 fi
+
+LIVE_URL=`get_config_value 'live_url' ''`
+if [ ! -z "$LIVE_URL" ]; then
+  # replace potential protocols, and remove trailing slashes
+  LIVE_URL=$(echo ${LIVE_URL} | sed 's|https://||' | sed 's|http://||'  | sed 's:/*$::')
+
+  redirect_config=$((cat <<END_HEREDOC
+if (!-e \$request_filename) {
+  rewrite ^/[_0-9a-zA-Z-]+(/wp-content/uploads/.*) \$1;
+}
+if (!-e \$request_filename) {
+  rewrite ^/wp-content/uploads/(.*)\$ \$scheme://${LIVE_URL}/wp-content/uploads/\$1 redirect;
+}
+END_HEREDOC
+
+  ) |
+  # pipe and escape new lines of the HEREDOC for usage in sed
+  sed -e ':a' -e 'N' -e '$!ba' -e 's/\n/\\n\\1/g'
+  )
+
+  sed -i -e "s|\(.*\){{LIVE_URL}}|\1${redirect_config}|" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+else
+  sed -i "s#{{LIVE_URL}}##" "${VVV_PATH_TO_SITE}/provision/vvv-nginx.conf"
+fi
+
+echo "Site Template provisioner script completed"
